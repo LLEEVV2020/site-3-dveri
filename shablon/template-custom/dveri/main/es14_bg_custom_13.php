8c6423bd23ec66e392a9376a36bee4cd1f3765f9
LLEEVV2020 <lleevv2020@gmail.com>
2019-07-08 13:06:39 +0400

    add

---
+++ shablon/template-custom/dveri/main/es14_bg_custom_13.php
@@ -0,0 +1,106 @@
+<div class="es14_bg_custom">
+    <div class="h2">
+        ОТЗЫВЫ НАШИХ ЗАКАЗЧИКОВ
+    </div>
+    <div class="container">
+        <section class="regular slider">
+            <div >
+                <div class="item-rewiews">
+                    <div class="box1">
+                        <img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/reviews1.png">
+                    </div>
+                    <div class="box2">
+                        <h5>Ольга Иваненко</h5>
+                        <p>Не могла долго решиться на такой глобальный для меня проект, как объединение балкона с комнатой, но все окаазалось гораздо проще, чем я представляла! Компания “Балконы цены” решила мой вопрос всего за две недели! Теперь у меня большая комната. 
+                                </p>
+                    </div>
+                </div>
+
+            </div>
+            <div >
+                <div class="item-rewiews">
+                    <div class="box1">
+                        <img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/reviews1.png">
+                    </div>
+                    <div class="box2">
+                        <h5>Максим Гладков </h5>
+                        <p>Не могла долго решиться на такой глобальный для меня проект, как объединение балкона с комнатой, но все окаазалось гораздо проще, чем я представляла! Компания “Балконы цены” решила мой вопрос всего за две недели! Теперь у меня большая комната. </p>
+                    </div>
+                </div>
+            </div>
+            <div >
+                <div class="item-rewiews">
+                    <div class="box1">
+                        <img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/reviews1.png">
+                    </div>
+                    <div class="box2">
+                        <h5>Вероника Сыроварова </h5>
+                        <p>Не могла долго решиться на такой глобальный для меня проект, как объединение балкона с комнатой, но все окаазалось гораздо проще, чем я представляла! Компания “Балконы цены” решила мой вопрос всего за две недели! Теперь у меня большая комната. </p>
+                    </div>
+                </div>
+
+            </div>
+            <div >
+                <div class="item-rewiews">
+                    <div class="box1">
+                        <img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/reviews1.png">
+                    </div>
+                    <div class="box2">
+                        <h5>Регина Казанцева </h5>
+                        <p>Не могла долго решиться на такой глобальный для меня проект, как объединение балкона с комнатой, но все окаазалось гораздо проще, чем я представляла! Компания “Балконы цены” решила мой вопрос всего за две недели! Теперь у меня большая комната.  </p>
+                    </div>
+                </div>
+
+            </div>
+            <div >
+                <div class="item-rewiews">
+                    <div class="box1">
+                        <img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/reviews1.png">
+                    </div>
+                    <div class="box2">
+                        <h5>Егор Василевич </h5>
+                        <p>Не могла долго решиться на такой глобальный для меня проект, как объединение балкона с комнатой, но все окаазалось гораздо проще, чем я представляла! Компания “Балконы цены” решила мой вопрос всего за две недели! Теперь у меня большая комната.  </p>
+                    </div> 
+                </div>
+
+            </div>
+            <div >
+                <div class="item-rewiews">
+                    <div class="box1">
+                        <img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/reviews1.png">
+                    </div>
+                    <div class="box2">
+                        <h5>Ольга Иваненко</h5>
+                        <p>Не могла долго решиться на такой глобальный для меня проект, как объединение балкона с комнатой, но все окаазалось гораздо проще, чем я представляла! Компания “Балконы цены” решила мой вопрос всего за две недели! Теперь у меня большая комната. 
+                                </p>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </div>
+</div>
+       
+               <script src="//<?php echo $_SERVER['SERVER_NAME']; ?>/js/slick.min.js"></script>
+ <script type="text/javascript">
+            $(document).ready(function(){
+                $(".regular").slick({
+                    dots: true,
+                    arrows: false,
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    responsive: [
+                        {
+                        breakpoint: 1200,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1
+                            }
+                        }
+                    ]
+                });
+            });
+            
+        </script>
+        
---
+++ shablon/template-custom/gde_kupit_shkafy_3/accii_temp.php
@@ -0,0 +1,49 @@
+   <div class="contact-wrap template contact-wrap-balcon">
+            <div class="container">
+                <h2>РАБОТАЕМ <?php echo get_option('my_city_po'); ?>:</h2>
+
+                <div class="flex-container vcard">
+                    
+                    <div class="block-item">
+                        <div class="block-img">
+                            <img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/contact-tel2.png" alt="">
+                        </div>
+                        <div class="item item-main">
+                           
+                           <div>Звоните ежедневно <br>с 8:00 до 23:00</div>
+                           
+                            
+                            <div class="tel"> <abbr class="value" title="8 (4752) 33-33-35">8 (4752) 33-33-35</abbr></div>
+                            <p class="email">ecookna68@mail.ru</p>
+                        </div>
+                    </div>
+                    <div class="block-item">
+                        <div class="block-img">
+                            <img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/ykazatel.png" alt="">
+                        </div>
+                        <div class="item">
+                            <div class="head">Центральный офис</div>
+                            <p class="workhours">Москва, Варшавское шоссе,
+д.33, этаж 4, офис 13, рядом со
+станцией метро Нагатинская</p>
+                        </div>
+                    </div>
+                    
+                    
+                    <div class="block-item">
+                        <div class="block-img">
+                            <img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/img/rekvizits.png" alt="">
+                        </div>
+                        <div class="item">
+                            <div class="head">Реквизиты</div>
+                            <p class="email">ООО “Надежный выбор”<br>
+ИНН 7726370558 <br>
+ОГРН 1167746224095 <br>
+Р/С 40702810038000019853</p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
