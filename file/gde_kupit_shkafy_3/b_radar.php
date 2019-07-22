cdf8fcfec3b939a154c0c865c08f0f1c1d15d278
Лев Милов <milov@ir-tech.ru>
2019-07-22 18:49:37 +0400

    add

---
+++ file/gde_kupit_shkafy_3/b_radar.php
@@ -0,0 +1,11 @@
+<div class="b-radar">
+   <!-- <div class="b-radar_bg" style="background: url(//<?php echo $_SERVER['SERVER_NAME']; ?>/city_include/<?php echo get_option('my_city_lat'); ?>/radar.jpg) no-repeat center center;">
+        <div class="b-radar_active">
+        </div>
+    </div>-->
+    <div class="b-radar_bg" style="background: url(//<?php echo $_SERVER['SERVER_NAME']; ?>/img//radar1.png) no-repeat center center;">
+        <div class="b-radar_active">
+        </div>
+    </div>
+</div>
+<!-- /.b-radar -->
